4608f2eb6f59be5686d2e3d55b6f2c7292d45a93
miruna-iosub <100404656+miruna-iosub@users.noreply.github.com>
2023-03-04 12:24:48 +0200

    Update Bonus.java

--- LAB1/Bonus.java
+++ LAB1/Bonus.java
@@ -1,4 +1,4 @@
-package Bonus;
+package bonus;
 
 import java.util.Scanner;
 
@@ -8,9 +8,11 @@ public class Bonus {
 
         System.out.println("Enter an integer: ");
         int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int degree = scanner.nextInt();
 
-        if (n <= 0) {
-            System.out.println("The integer must be positive.");
+        if (n <= 0 || m <= 0 || degree <= 0) {
+            System.out.println("The integers must be positive.");
             return;
         }
 
@@ -29,9 +31,9 @@ public class Bonus {
         }
 
         int[][] B = A;
-        int [][] C = new int[n][n];
+        int[][] C = new int[n][n];
         for (int i = 2; i <= n; i++) {
-            C = Multiply(A,B);
+            C = multiply(A, B);
             System.out.println("A^" + i);
 
             for (int j = 0; j < n; j++) {
@@ -41,9 +43,22 @@ public class Bonus {
                 System.out.println();
             }
         }
+
+
+        int[][] D = new int[m][degree];
+        D = createAdjacencyMatrix(m, degree);
+
+        System.out.println("Regular adjacency matrix");
+        for (int j = 0; j < m; j++) {
+            for (int k = 0; k < m; k++) {
+                System.out.print(D[j][k] + " ");
+            }
+            System.out.println();
+        }
+
     }
 
-    public static int[][] Multiply(int[][] A, int [][] B) {
+    public static int[][] multiply(int[][] A, int[][] B) {
         int n = A.length;
         int[][] C = new int[n][n];
         for (int i = 0; i < n; i++) {
@@ -54,5 +69,26 @@ public class Bonus {
             }
         }
         return C;
+    }
+
+    public static int[][] createAdjacencyMatrix(int vertices, int degree) {
+        int[][] matrix = new int[vertices][vertices];
+
+        int isDegreeEven = 1;
+        if (degree % 2 != 0) isDegreeEven = 1;
+
+        int edges = degree / 2;
+        if (isDegreeEven == 0) {
+            edges++;
+        }
+
+        for (int i = 0; i < vertices; i++) {
+            for (int j = 1; j <= edges; j++) {
+                int neighborIndex = (i + j) % vertices;
+                matrix[i][neighborIndex] = 1;
+                matrix[neighborIndex][i] = 1;
+            }
+        }
+        return matrix;
     }
 }
