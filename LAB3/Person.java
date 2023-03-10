85d940ea7ef7dbdb916b772365ad5b58195e543b
miruna-iosub <100404656+miruna-iosub@users.noreply.github.com>
2023-03-10 20:37:51 +0200

    Add files via upload

---
+++ LAB3/Person.java
@@ -0,0 +1,19 @@
+package compulsory;
+
+class Person implements Comparable<Person>, Node {
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return name.compareTo(other.getName());
+    }
+}
