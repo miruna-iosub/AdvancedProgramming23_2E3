18758359d380ba9df0d9260d252c35cf26b032b4
miruna-iosub <100404656+miruna-iosub@users.noreply.github.com>
2023-03-24 20:33:48 +0200

    Update CatalogUtil.java

--- LAB5/org.compulsory/CatalogUtil.java
+++ LAB5/org.compulsory/CatalogUtil.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
-
+ 
 public class CatalogUtil {
     public static void save(Catalog catalog, String path) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
