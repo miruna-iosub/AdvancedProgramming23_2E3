bb40d67c2700936e9f599d2f6ae80ff1148e2dc8
miruna-iosub <100404656+miruna-iosub@users.noreply.github.com>
2023-03-24 20:33:58 +0200

    Update Compulsory.java

--- LAB5/org.compulsory/Compulsory.java
+++ LAB5/org.compulsory/Compulsory.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Compulsory {
     public static void main(String[] args) throws IOException {
         Catalog catalog = new Catalog("Catalog");
-
+ 
         Document doc1 = new Document("1", "Compulsory Java", "Compulsory.java");
         doc1.addTag("year", 2023);
         doc1.addTag("author", "Miruna Iosub");
