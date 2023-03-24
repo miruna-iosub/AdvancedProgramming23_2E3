11fee93b41e955a4964c4c2deb8b0bbe287adb11
miruna-iosub <100404656+miruna-iosub@users.noreply.github.com>
2023-03-24 19:56:48 +0200

    Update Network.java

--- LAB3/Bonus/Network.java
+++ LAB3/Bonus/Network.java
@@ -3,6 +3,7 @@ package bonus;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.*;
 
 public class Network {
     private final List<Object> nodes;
@@ -53,5 +54,55 @@ public class Network {
             }
         }
         return totalConnected;
+    }
+
+    public List<List<Object>> identifyBlocks() {
+        List<List<Object>> blocks = new ArrayList<>();
+        Set<Object> visited = new HashSet<>();
+        for (Object node : nodes) {
+            if (!visited.contains(node)) {
+                List<Object> block = new ArrayList<>();
+                blocks.add(block);
+                visit(node, visited, block);
+            }
+        }
+        return blocks;
+    }
+
+    private void visit(Object node, Set<Object> visited, List<Object> block) {
+        visited.add(node);
+        block.add(node);
+        List<Object> neighbors = getNeighbors(node);
+        for (Object neighbor : neighbors) {
+            if (!visited.contains(neighbor) && isTwoConnected(node, neighbor)) {
+                visit(neighbor, visited, block);
+            }
+        }
+    }
+
+    private boolean isTwoConnected(Object node1, Object node2) {
+        List<Object> neighbors1 = getNeighbors(node1);
+        List<Object> neighbors2 = getNeighbors(node2);
+        neighbors1.remove(node2);
+        neighbors2.remove(node1);
+        Set<Object> commonNeighbors = new HashSet<>(neighbors1);
+        commonNeighbors.retainAll(neighbors2);
+        return commonNeighbors.size() >= 1;
+    }
+
+    private List<Object> getNeighbors(Object node) {
+        List<Object> neighbors = new ArrayList<>();
+        if (node instanceof Person) {
+            Map<String, Object> personsImportance = ((Person) node).getRelationships();
+            for (Object neighbor : personsImportance.values()) {
+                    neighbors.add(neighbor);
+            }
+        } else {
+            Map<String, Object> companiesImportance = ((Company) node).getRelationships();
+            for (Object neighbor : companiesImportance.values()) {
+                    neighbors.add(neighbor);
+            }
+        }
+        return neighbors;
     }
 }
