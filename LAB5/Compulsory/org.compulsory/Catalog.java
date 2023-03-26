d5a2217bcb53f9b0bae8fcd53a3c45d1975bb223
miruna-iosub <100404656+miruna-iosub@users.noreply.github.com>
2023-03-26 11:50:42 +0300

    Updated LAB5

---
+++ LAB5/Compulsory/org.compulsory/Catalog.java
@@ -0,0 +1,42 @@
+package org.compulsory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.Serializable;
+public class Catalog implements Serializable {
+    private String name;
+    private List<Document> docs = new ArrayList<>();
+    public Catalog(){};
+    public Catalog(String name){
+        this.name=name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<Document> getDoc() {
+        return docs;
+    }
+    public void setDoc(List<Document> docs) {
+        this.docs = docs;
+    }
+
+    public void add(Document doc) {
+        docs.add(doc);
+    }
+
+    public Document findById(String id) {
+        return docs.stream()
+                .filter(d -> d.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    @Override
+    public String toString() {
+        return "Catalog{" +
+                "name='" + name + '\'' +
+                ", doc=" + docs +
+                '}';
+    }
+}
