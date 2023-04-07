9c4895ca76f0e92db2aae43873ed47c76221298f
miruna-iosub <100404656+miruna-iosub@users.noreply.github.com>
2023-04-07 11:46:14 +0300

    Added compulsory files

---
+++ LAB7/Compulsory/Token.java
@@ -0,0 +1,11 @@
+package org.compulsory;
+
+public class Token {
+    private final int value;
+    public Token(int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
+}
