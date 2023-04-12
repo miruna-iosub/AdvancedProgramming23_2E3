51c81cf8b2aebf2cdf91cfe56230766983f89f97
miruna-iosub <100404656+miruna-iosub@users.noreply.github.com>
2023-04-12 21:16:28 +0300

    Added module-info.java

---
+++ LAB6/Homework/module-info.java
@@ -0,0 +1,19 @@
+module com.example.demo {
+        requires javafx.controls;
+        requires javafx.fxml;
+        requires javafx.web;
+        requires javafx.swing;
+        requires java.desktop;
+
+        requires org.controlsfx.controls;
+        requires com.dlsc.formsfx;
+        requires validatorfx;
+        requires org.kordamp.ikonli.javafx;
+        requires org.kordamp.bootstrapfx.core;
+        requires eu.hansolo.tilesfx;
+        requires com.google.gson;
+
+        opens com.lab6.homework.files to com.google.gson;
+        opens com.lab6.homework to javafx.fxml;
+        exports com.lab6.homework;
+        }
