ed58b211187c6b2aa8572b44e56735180f809679
miruna-iosub <100404656+miruna-iosub@users.noreply.github.com>
2023-05-07 13:33:22 +0300

    Added test

---
+++ LAB9/Compulsory/src/main/java/app/Main.java
@@ -0,0 +1,26 @@
+package app;
+
+import java.util.List;
+import entity.Album;
+import entity.Artist;
+import repository.AlbumRepository;
+import repository.ArtistRepository;
+
+public class Main {
+
+    private static List<Artist> artists;
+    private static List<Album> albums;
+
+    public static void main(String[] args) {
+
+        System.out.println("THE ARTIST WITH ID 1 IS: ");
+        System.out.println(ArtistRepository.findById(1));
+
+        System.out.println("THE ARTISTS WITH NAME STARTING WITH THE LETTER T: ");
+        artists = ArtistRepository.findByName("M%");
+
+        System.out.println("THE FIRST ALBUM IS: ");
+        System.out.println(AlbumRepository.findById(1));
+
+    }
+}
