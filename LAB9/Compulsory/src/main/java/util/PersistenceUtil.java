e7dec1cc486b6b15220b02bbf53ee5403a898fe9
miruna-iosub <100404656+miruna-iosub@users.noreply.github.com>
2023-05-07 13:32:45 +0300

    Added persistence class

---
+++ LAB9/Compulsory/src/main/java/util/PersistenceUtil.java
@@ -0,0 +1,27 @@
+package util;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class PersistenceUtil {
+
+    private EntityManagerFactory entity_manager_factory = null;
+    private static PersistenceUtil instance;
+
+    public EntityManagerFactory getEMFactory() {
+
+        return entity_manager_factory;
+    }
+
+    private PersistenceUtil() {
+
+        entity_manager_factory = Persistence.createEntityManagerFactory("AlbumsPU");
+    }
+
+    public static PersistenceUtil getInstance() {
+
+        if (null == instance)
+            instance = new PersistenceUtil();
+        return instance;
+    }
+}
