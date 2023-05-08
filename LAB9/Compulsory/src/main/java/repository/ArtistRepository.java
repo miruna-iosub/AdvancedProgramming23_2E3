3c6e454b0b8c8507ed3d69bed6c1ac25c85db8bc
miruna-iosub <100404656+miruna-iosub@users.noreply.github.com>
2023-05-08 21:53:38 +0300

    Update ArtistRepository.java

--- LAB9/Compulsory/src/main/java/repository/ArtistRepository.java
+++ LAB9/Compulsory/src/main/java/repository/ArtistRepository.java
@@ -13,32 +13,32 @@ import javax.persistence.Query;
 public class ArtistRepository {
     public static void create(Artist artist){
 
-        EntityManagerFactory entity_manager_factory = PersistenceUtil.getInstance().getEMFactory();
-        EntityManager entity_manager = entity_manager_factory.createEntityManager();
-        entity_manager.getTransaction().begin();
+        EntityManagerFactory entityManagerFactory = PersistenceUtil.getInstance().getEMFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
 
-        entity_manager.persist(artist);
-        entity_manager.getTransaction().commit();
-        entity_manager.close();
+        entityManager.persist(artist);
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
 
     public static Artist findById(int id){
 
-        EntityManagerFactory entity_manager_factory = PersistenceUtil.getInstance().getEMFactory();
-        EntityManager entity_manager = entity_manager_factory.createEntityManager();
-        Artist artist = entity_manager.find(Artist.class, id);
-        entity_manager.close();
+        EntityManagerFactory entityManagerFactory = PersistenceUtil.getInstance().getEMFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Artist artist = entityManager.find(Artist.class, id);
+        entityManager.close();
 
         return artist;
     }
 
     public static List<Artist> findByName(String name){
-        EntityManagerFactory entity_manager_factory = PersistenceUtil.getInstance().getEMFactory();
-        EntityManager entity_manager = entity_manager_factory.createEntityManager();
-        Query query = entity_manager.createQuery(String.format("SELECT e FROM artists e WHERE e.name LIKE '%e%'", name));
-        List<Artist> list_of_artists = (List<Artist>) query.getResultList();
-        entity_manager.close();
+        EntityManagerFactory entityManagerFactory = PersistenceUtil.getInstance().getEMFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Query query = entityManager.createQuery(String.format("SELECT e FROM artists e WHERE e.name LIKE '%e%'", name));
+        List<Artist> listOfartists = (List<Artist>) query.getResultList();
+        entityManager.close();
 
-        return list_of_artists;
+        return listOfartists;
     }
 }
