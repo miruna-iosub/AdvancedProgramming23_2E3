190edf4b73224a3b141ad6224411b757515c8a8b
miruna-iosub <100404656+miruna-iosub@users.noreply.github.com>
2023-05-14 10:42:17 +0300

    Update GameClient.java

--- LAB10/Compulsory/Client/GameClient.java
+++ LAB10/Compulsory/Client/GameClient.java
@@ -3,7 +3,7 @@ package org.compulsory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.PrintWriter; 
 import java.net.Socket;
 import java.net.UnknownHostException;
 
